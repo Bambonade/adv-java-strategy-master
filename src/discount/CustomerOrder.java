ee916e5fcd524856d22d2d1a680a3bc012aecde0
Brandon Nelson <machinecar998@gmail.com>
2022-11-29 14:15:53 -0600

    inital commit

---
+++ src/discount/CustomerOrder.java
@@ -0,0 +1,44 @@
+package discount;
+
+public class CustomerOrder {
+
+    private String customerName;
+    private double billAmount;
+    private DiscountStrategy discountStrategy;
+
+    public CustomerOrder(String customerName, double billAmount, DiscountStrategy discountStrategy) {
+        this.customerName = customerName;
+        this.billAmount = billAmount;
+        this.discountStrategy = discountStrategy;
+    }
+
+    public double getBillAmount() {
+        return billAmount;
+    }
+
+    public void setBillAmount(double billAmount) {
+        this.billAmount = billAmount;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public void discount(){
+        if(discountStrategy != null){
+            billAmount = discountStrategy.discount(billAmount);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{"
+                + "customerName='" + customerName + '\''
+                + ", billAmount=" + billAmount
+                + '}';
+    }
+}
